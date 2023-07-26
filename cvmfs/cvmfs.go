7d7fd9011ddb8dddf66d39572dcd82f8cb8aab1a
marcoverl <marco.verlato@pd.infn.it>
2023-07-26 16:24:52 +0200

    Update cvmfs.go
    Code to avoid null layer

--- cvmfs/cvmfs.go
+++ cvmfs/cvmfs.go
@@ -62,6 +62,14 @@ func (fs *Filesystem) Mount(ctx context.Context, mountpoint string, labels map[s
 		return err
 	}
 	digest = strings.Split(digest, ":")[1]
+
+	// Check if the digest is the one to avoid mounting
+	if digest == "4f4fb700ef54461cfa02571ae0db9a0dc1e0cdb5577484a6d75e68dc38e8acc1" {
+		err := fmt.Errorf("mounting layer %s is not allowed", digest)
+		log.G(ctx).WithError(err).WithField("layer digest", digest).Debug("cvmfs: Layer mounting not allowed")
+		return err
+	}
+	
 	firstTwo := digest[0:2]
 	path := filepath.Join(fs.fsAbsoluteMountpoint, ".layers", firstTwo, digest, "layerfs")
 	if _, err := os.Stat(path); os.IsNotExist(err) {
