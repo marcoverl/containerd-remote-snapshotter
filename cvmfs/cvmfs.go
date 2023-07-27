b9c1a6125e3c0622c114a8c33cea3fb671e24609
marcoverl <marco.verlato@pd.infn.it>
2023-07-27 18:00:47 +0200

    Update cvmfs.go

--- cvmfs/cvmfs.go
+++ cvmfs/cvmfs.go
@@ -53,14 +53,14 @@ func NewFilesystem(ctx context.Context, root string, config *Config) (snapshot.F
 	return &Filesystem{fsAbsoluteMountpoint: absolutePath, mountedLayers: mountedLayersMap}, nil
 }
 
-func isPathMounted(fs *Filesystem, path string) bool {
-    for _, mountedPath := range fs.mountedLayers {
-        if mountedPath == path {
-            return true
-        }
-    }
-    return false
-}
+//func isPathMounted(fs *Filesystem, path string) bool {
+//    for _, mountedPath := range fs.mountedLayers {
+//        if mountedPath == path {
+//            return true
+//        }
+//    }
+//    return false
+//}
 
 func (fs *Filesystem) Mount(ctx context.Context, mountpoint string, labels map[string]string) error {
 	log.G(ctx).Info("Mount layer from cvmfs")
@@ -71,6 +71,14 @@ func (fs *Filesystem) Mount(ctx context.Context, mountpoint string, labels map[s
 		return err
 	}
 	digest = strings.Split(digest, ":")[1]
+
+	// Check if the digest is the one to not mount
+	if digest == "4f4fb700ef54461cfa02571ae0db9a0dc1e0cdb5577484a6d75e68dc38e8acc1" {
+		err := fmt.Errorf("mounting layer %s is not allowed", digest)
+		log.G(ctx).WithError(err).WithField("layer digest", digest).Debug("cvmfs: Layer mounting not allowed")
+		return err
+	}
+	
 	firstTwo := digest[0:2]
 	path := filepath.Join(fs.fsAbsoluteMountpoint, ".layers", firstTwo, digest, "layerfs")
 	if _, err := os.Stat(path); os.IsNotExist(err) {
@@ -81,11 +89,11 @@ func (fs *Filesystem) Mount(ctx context.Context, mountpoint string, labels map[s
 	log.G(ctx).WithField("layer digest", digest).Debug("cvmfs: Layer present in CVMFS")
 	
         // Check if the layer is already mounted
-        if isPathMounted(fs, path) {
-            err := fmt.Errorf("layer %s is already mounted", digest)
-	    log.G(ctx).WithError(err).WithField("layer digest", digest).Debug("cvmfs: Layer was already mounted")
-            return err
-	}
+        //if isPathMounted(fs, path) {
+        //    err := fmt.Errorf("layer %s is already mounted", digest)
+	//    log.G(ctx).WithError(err).WithField("layer digest", digest).Debug("cvmfs: Layer was already mounted")
+        //    return err
+	//}
 	
 	err := syscall.Mount(path, mountpoint, "", syscall.MS_BIND, "")
 	if err != nil {
